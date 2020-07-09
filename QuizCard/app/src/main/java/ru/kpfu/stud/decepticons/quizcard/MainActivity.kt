ec870d7a9acbeceb45d99e42d8dde214c9f1c50b
rishat19 <57725670+rishat19@users.noreply.github.com>
2020-07-09 20:07:59 +0300

    Empty project

---
+++ QuizCard/app/src/main/java/ru/kpfu/stud/decepticons/quizcard/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.kpfu.stud.decepticons.quizcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
